c7da5b57b8036d788462c1b05adc72438dcbbf04
Adnan Mouslli <yazanmouslli6@gmail.com>
2024-08-15 02:14:07 -0700

    update get data home

--- home/get_data_home.php
+++ home/get_data_home.php
@@ -37,10 +37,24 @@ $data4 = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $count4  = $stmt->rowCount();
 
 
+$stmt = $con->prepare(
+    "SELECT f.* FROM `farms` f 
+    join `farm-evaluation` e 
+    on f.id = e.id_farm 
+    where status = 1
+    GROUP by f.id 
+    ORDER BY SUM(e.evaluation) / COUNT(e.evaluation) DESC 
+    LIMIT 1;
+    ");
+    
+    $stmt->execute();
+    $data5 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $count5  = $stmt->rowCount();
+
 if ($count1 > 0) {
-    echo json_encode(array("status" => "success", "data1" => $data1 , "data2" => $data2 , "data3" => $data3 , "data4" => $data4));
+    echo json_encode(array("status" => "success", "data1" => $data1 , "data2" => $data2 , "data3" => $data3 , "data4" => $data4 , "data5" => $data5));
 } else {
     echo json_encode(array("status" => "failure"));
 }
 
-?>
+
